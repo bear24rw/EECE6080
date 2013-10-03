817e53ddfa798a160bc93b552d5cb0860fc7f660
qixiaohui <qixiaohuihaha@gmail.com>
2013-10-03 19:27:45 -0400

    new added spice file

---
+++ hw_3/part_3/xiaohui/bitslice.cmd
@@ -0,0 +1,13 @@
+stepsize 10
+analyzer A B cin S F cout
+vector vec1 A B cin S
+setvector vec1 1100
+s
+setvector vec1 0011
+s 
+setvector vec1 1111
+s
+setvector vec1 0111
+s 
+setvector vec1 1000
+s
