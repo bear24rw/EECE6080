954dfff7d4da03c2f4c69432ac9b7c0befe3173f
Max Thrun <bear24rw@gmail.com>
2013-10-02 23:07:35 -0400

    irsim files and pics

---
+++ hw_3/part_3/part_3.cmd
@@ -0,0 +1,39 @@
+h VDD
+l GND
+
+l A B C_IN S
+vector in A B C_IN S
+ana A B C_IN S C_OUT F
+
+setvector in 0000
+s
+setvector in 0001
+s
+setvector in 0010
+s
+setvector in 0011
+s
+setvector in 0100
+s
+setvector in 0101
+s
+setvector in 0110
+s
+setvector in 0111
+s
+setvector in 1000
+s
+setvector in 1001
+s
+setvector in 1010
+s
+setvector in 1011
+s
+setvector in 1100
+s
+setvector in 1101
+s
+setvector in 1110
+s
+setvector in 1111
+s
