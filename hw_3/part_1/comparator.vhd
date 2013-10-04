c52a621f1d959adf972a319843e87047fb8d7d2d
Max Thrun <bear24rw@gmail.com>
2013-10-03 23:44:13 -0400

    fixed syntax style on part 1

--- hw_3/part_1/comparator.vhd
+++ hw_3/part_1/comparator.vhd
@@ -2,20 +2,22 @@ library IEEE;
 use ieee.std_logic_1164.all;
 
 entity comparator is
-generic (n : integer:=7);
-port(
-	a,b: in std_logic_vector(n downto 0);
-	c  : out std_logic_vector(n downto 0)
-);
+    generic(
+        n : integer := 7
+    );
+    port(
+        a,b : in std_logic_vector(n downto 0);
+        c   : out std_logic_vector(n downto 0)
+    );
 end entity;
+
 architecture arc of comparator is
-	begin
-		process (a,b)
-		begin
-		if(a<b)
-		then c<=a;
-		elsif(a>=b)
-		then c<=b;
-	   end if;
-	end process;
+begin
+    process (a,b) begin
+        if(a<b) then
+            c <= a;
+        else
+            c <= b;
+        end if;
+    end process;
 end arc;
