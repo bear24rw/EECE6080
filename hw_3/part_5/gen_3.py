96b730f70fa3036e0bb91402bcc641d66ad726ea
Max Thrun <bear24rw@gmail.com>
2013-10-03 21:32:34 -0400

    part 5 with 3 slices

---
+++ hw_3/part_5/gen_3.py
@@ -0,0 +1,6 @@
+for a in range(0, 2**3):
+    for b in range(0, 2**3):
+        print "setvector A %s" % (bin(a).replace("0b","").zfill(3))
+        print "setvector B %s" % (bin(b).replace("0b","").zfill(3))
+        print "s"
+        print "path F"
---
+++ hw_3/part_5/run_irsim_3.sh
@@ -0,0 +1,1 @@
+irsim -s../../models/scmos30.prm fun_3.sim -fun_3.cmd
