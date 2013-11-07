4be574d78fdbdec87303651d84ed187dbfb506da
Max Thrun <bear24rw@gmail.com>
2013-11-06 23:56:54 -0500

    [irsim] use asserts in top test mode. manually edit cmd file since its straight foward

---
+++ project/irsim/top_test.cmd
@@ -0,0 +1,39 @@
+stepsize 10
+logfile top_test.log
+h VDD
+l GND
+vector CLK SCLKO
+clock  CLK 0 1
+w   SCLKO SI SSO SO
+ana SCLKO SI SSO SO
+l LDO
+l PCLKI
+l PSI
+
+| enable test mode
+h TESTO
+
+| clock in a pulse (010)
+l SI
+c
+h SI
+c
+l SI
+c
+
+| clock it until the pulse is just about to come out
+| 15*15+15-3-1 == 236
+|
+| 15*15+15 == number of flip flops
+| 3 == we already clocked three times
+| 1 == stop right before the pulse comes out
+c 236
+
+| make sure we see the pulse
+assert SO 0
+c
+assert SO 1
+c
+assert SO 0
+c
+
--- project/irsim/top_test.sh
+++ project/irsim/top_test.sh
@@ -1,2 +1,1 @@
-python top_test.py
 irsim -s../../models/scmos30.prm ../magic/top.sim -top_test.cmd
